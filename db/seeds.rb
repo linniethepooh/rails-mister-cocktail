4404486806e9cc738167298733a29f10b0989c12
Lindelwe Ncube <linniencube@gmail.com>
2018-07-30 16:09:32 +0800

    completed backend of three moddel cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "whiskey")
+Ingredient.create(name: "orange juice")
+Ingredient.create(name: "lemonade")
